bbf00f61131a267088501578dde38832af33a557
91084872 <31664295+91084872@users.noreply.github.com>
2017-10-02 10:28:09 -0700

    Worked on hiLow

--- src/org/teachingkidsprogramming/section03ifs/HiLow2.java
+++ src/org/teachingkidsprogramming/section03ifs/HiLow2.java
@@ -3,13 +3,15 @@ package org.teachingkidsprogramming.section03ifs;
 import org.teachingextensions.logo.Sound;
 import org.teachingextensions.logo.utils.EventUtils.MessageBox;
 
-public class HiLow
+public class HiLow2
 {
   public static void main(String[] args)
   {
     //int answer = 11;
+    int number;
+    number = MessageBox.askForNumericalInput("How many guesses do you need?");
     int answer = (int) (Math.random() * 100 + 1);
-    for (int i = 1; i <= 8; i++)
+    for (int i = 1; i <= number; i++)
     {
       int guess = MessageBox.askForNumericalInput("what is your guess?");
       if (guess == answer)
@@ -26,7 +28,7 @@ public class HiLow
       {
         MessageBox.showMessage("Too low");
       }
-      if (i == 8)
+      if (i == number)
       {
         MessageBox.showMessage("You Lost");
       }
