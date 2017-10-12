bf5cf70e13833f66148396b680a63e99b86d7213
91084872 <31664295+91084872@users.noreply.github.com>
2017-10-12 14:30:40 -0700

    Continued

--- src/org/teachingkidsprogramming/section03ifs/ChooseYourOwnAdventure.java
+++ src/org/teachingkidsprogramming/section03ifs/ChooseYourOwnAdventure.java
@@ -47,23 +47,7 @@ public class ChooseYourOwnAdventure
     {
       //            pourIntoToilet (recipe below) --#12.1
       //            ------------- Recipe for pourIntoToilet --#12.2
-      //               Tell the user "As you pour the ooze into the toilet it backs up, gurgles, and explodes, covering you in radioactive waste." --#10
-      MessageBox.showMessage(
-          "As you pour the ooze into the toilet it backs up, gurgles, and explodes, covering you in radioactive waste.");
-      //               Ask the user "Do you want to train to be a NINJA?  'Yes' or 'HECK YES'?" --#11
-      MessageBox.askForTextInput("Do you want to train to be a NINJA?  'Yes' or 'HECK YES'?");
-      //               If they answer "yes" --#13.1
-      if ("Yes".equals(answer))
-      {
-        //                    Tell the user "Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!" --#14
-        MessageBox
-            .showMessage("Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!");
-      }
-      //               Otherwise, if they answer "heck yes" --#13.2
-      //                    Tell the user "Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!" --#15
-      //               Otherwise, if they answer anything else --#13.3
-      //                    endStory --#16
-      //            ------------- End of pourIntoToilet recipe --#12.3
+      pourIntoToilet(answer);
     }
     //         Otherwise, if they answer "backyard" --#8.2
     else if ("backyard".equals(answer))
@@ -86,6 +70,32 @@ public class ChooseYourOwnAdventure
       //            endStory --#9
       endStory();
     }
+  }
+  private static void pourIntoToilet(int answer)
+  {
+    MessageBox.showMessage(
+        "As you pour the ooze into the toilet it backs up, gurgles, and explodes, covering you in radioactive waste.");
+    String answe = MessageBox.askForTextInput("Do you want to train to be a NINJA?  'Yes' or 'HECK YES'?");
+    if ("Yes".equals(answe))
+    {
+      //                    Tell the user "Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!" --#14
+      MessageBox
+          .showMessage("Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!");
+    }
+    //               Otherwise, if they answer "heck yes" --#13.2
+    else if ("heck yes".equals(answe))
+    {
+      MessageBox
+          .showMessage("Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!");
+    }
+    //                    Tell the user "Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!" --#15
+    //               Otherwise, if they answer anything else --#13.3
+    else
+    {
+      endStory();
+    }
+    //                    endStory --#16
+    //            ------------- End of pourIntoToilet recipe --#12.3
   }
   private static void wakeUp()
   {
