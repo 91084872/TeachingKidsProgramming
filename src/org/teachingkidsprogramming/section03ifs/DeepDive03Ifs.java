f871a983d9ccb23764fbd47fdde315f3746e0776
91084872 <31664295+91084872@users.noreply.github.com>
2017-10-27 13:32:21 -0700

    save code

--- src/org/teachingkidsprogramming/section03ifs/DeepDive03Ifs.java
+++ src/org/teachingkidsprogramming/section03ifs/DeepDive03Ifs.java
@@ -21,7 +21,7 @@ public class DeepDive03Ifs
     String bearPoopPlace = "";
     if (true)
     {
-      bearPoopPlace = "";
+      bearPoopPlace = "woods";
     }
     Assert.assertEquals("woods", bearPoopPlace);
   }
@@ -49,7 +49,7 @@ public class DeepDive03Ifs
   public void isThePopeCatholic() throws Exception
   {
     String pope = "";
-    if (_____)
+    if (true)
     {
       pope = "Catholic";
     }
@@ -59,7 +59,7 @@ public class DeepDive03Ifs
   public void trueOrFalse() throws Exception
   {
     String animal = "cat";
-    boolean elephant = _____;
+    boolean elephant = true;
     if (elephant)
     {
       animal = "flat " + animal;
@@ -70,7 +70,7 @@ public class DeepDive03Ifs
   public void letSleepingBabiesLie() throws Exception
   {
     String babySounds = "";
-    boolean sleeping = ______;
+    boolean sleeping = false;
     if (sleeping)
     {
       babySounds = "zzzzzzzzzzzz";
@@ -85,7 +85,7 @@ public class DeepDive03Ifs
   public void howCoachThinks() throws Exception
   {
     String coachSays = "try harder";
-    int percentEffort = ____;
+    int percentEffort = 110;
     if (percentEffort == 110)
     {
       coachSays = "good job";
@@ -96,7 +96,7 @@ public class DeepDive03Ifs
   public void lessThan() throws Exception
   {
     String modeOfTransportation = "";
-    int age = ____;
+    int age = 15;
     if (age < 16)
     {
       modeOfTransportation = "keep walking";
@@ -111,7 +111,7 @@ public class DeepDive03Ifs
   public void greaterThan() throws Exception
   {
     String kidSays = "";
-    int numberOfIceCreams = ____;
+    int numberOfIceCreams = 5;
     if (numberOfIceCreams > 4)
     {
       kidSays = "I think I'm gonna barf";
@@ -126,8 +126,8 @@ public class DeepDive03Ifs
   public void notEqual() throws Exception
   {
     String playerSays = "";
-    int cards = ____;
-    if (cards != 52)
+    int cards = 51;
+    if (cards != 51)
     {
       playerSays = "Not playing with a full deck!";
     }
@@ -141,7 +141,7 @@ public class DeepDive03Ifs
   public void equalsForStrings() throws Exception
   {
     String knockKnock = "";
-    String whosThere = ___;
+    String whosThere = "bananas";
     if (whosThere.equals("bananas"))
     {
       knockKnock = "Who's there?";
@@ -156,8 +156,8 @@ public class DeepDive03Ifs
   public void thisAndThat() throws Exception
   {
     String time = "";
-    int score = ____;
-    int years = ____;
+    int score = 4;
+    int years = 7;
     if (score == 4 && years == 7)
     {
       time = "Presidential";
@@ -168,7 +168,7 @@ public class DeepDive03Ifs
   public void theBeginningOrEnd() throws Exception
   {
     String shoppingList = "";
-    int age = ____;
+    int age = 2;
     if (age <= 2 || 90 <= age)
     {
       shoppingList = "diapers";
@@ -179,7 +179,7 @@ public class DeepDive03Ifs
   public void ifInHighSchool() throws Exception
   {
     String status = "";
-    int age = ____;
+    int age = 16;
     if (age <= 15)
     {
       status = "smarty";
@@ -199,7 +199,7 @@ public class DeepDive03Ifs
   {
     String status = "";
     String animal = "PIG";
-    boolean isWinningKarate = ______;
+    boolean isWinningKarate = false;
     if (animal.equalsIgnoreCase("pig"))
     {
       if (isWinningKarate)
@@ -218,11 +218,53 @@ public class DeepDive03Ifs
   {
     String dessert = "chocolate";
     if (false)
-      ;
     {
       dessert = "ketchup";
     }
-    Assert.assertEquals(___, dessert);
+    Assert.assertEquals("chocolate", dessert);
+  }
+  @Test
+  public void cyoaWakeUpTest() throws Exception
+  {
+    String result = MessageBox.askForTextInput("Do you want to 'wake up' or 'explore' the dream?");
+    //test that user entered "Hello"
+    Assert.assertEquals(result, "wake up");
+  }
+  @Test
+  public void test2() throws Exception
+  {
+    String result = MessageBox.askForTextInput("Do you want to 'wake up' or 'explore' the dream?");
+    Assert.assertEquals(result, "explore");
+  }
+  @Test
+  public void cyoaNoAnswerTest2() throws Exception
+  {
+    String result = MessageBox.askForTextInput("Do you want to 'wake up' or 'explore' the dream?");
+    Assert.assertEquals("", result);
+  }
+  @Test
+  public void teacherSays() throws Exception
+  {
+    String teacherSays = "may";
+    if (_____)
+    {
+      teacherSays = "can";
+    }
+    Assert.assertEquals("may", teacherSays);
+  }
+  @Test
+  public void GuessMyAge() throws Exception
+  {
+    int seven = ____;
+    if (seven == 7)
+    {
+      System.out.print("Correct");
+    }
+    else
+    {
+      System.out.println("Incorrect");
+    }
+    Assert.assertEquals(____, "Correct");
   }
   /**
    * Ignore the following, It's needed to run the deep dive
@@ -245,41 +287,4 @@ public class DeepDive03Ifs
   {
     return ___;
   }
-  @Test
-public void cyoaInputTest2() throws Exception
-{
-String result = MessageBox.askForTextInput("Do you want to 'wake up' or 'explore' the dream?");
-//test that user entered "Hello"
- 
-Assert.assertEquals(result, "wake up");
-}
-  @Test
-  public void test2()throws Exception
-  {
-    String result = MessageBox.askForTextInput("Do you want to 'wake up' or 'explore' the dream?");
-    Assert.assertEquals(result, "explore");
-  }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-  }  
-  
-  
 }
--- src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
+++ src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
@@ -6,8 +6,10 @@ public class PentagonCrazy
 {
   public static void main(String[] args)
   {
+    int length = 50;
     Tortoise.show();
     //    Make the tortoise move as fast as possible --#3
+    Tortoise.setSpeed(10);
     //    createColorPalette (recipe below) --#8.1
     //
     //    ------------- Recipe for createColorPalette --#8.2
@@ -22,20 +24,25 @@ public class PentagonCrazy
     //
     //    ------------- Recipe for drawPentagon --#10.2
     //    Do the following 200 times --#2.1
-    //        adjustPen (recipe below) --#9.1
-    //
-    //        ------------- Recipe for adjustPen --#9.2
-    //        Change the pen color of the line the tortoise draws to the next color on the color wheel --#6
-    //        Increase the tortoises pen width by 1 --#15                                              
-    //        If the tortoise's pen width is greater than 4, then --#17
-    //            Reset the pen width to 1 --#16
-    //        ------------- End of adjustPen recipe --#9.3 
-    //
-    //        The current length of a side is the same as the number of the side you are about to draw ( 1st side = 1 pixel, 2nd side = 2 pixels, etc) --#4.2
-    //        Move the tortoise the length of a side --#4.1
-    //        Turn the tortoise 1/5th of 360 degrees --#1
-    //        Turn the tortoise 1 more degree --#5
-    //    End Repeat --#2.2
+    for (int i = 1; i <= 200; i++)
+    {
+      //        adjustPen (recipe below) --#9.1
+      //
+      //        ------------- Recipe for adjustPen --#9.2
+      //        Change the pen color of the line the tortoise draws to the next color on the color wheel --#6
+      //        Increase the tortoises pen width by 1 --#15                                              
+      //        If the tortoise's pen width is greater than 4, then --#17
+      //            Reset the pen width to 1 --#16
+      //        ------------- End of adjustPen recipe --#9.3 
+      //
+      //        The current length of a side is the same as the number of the side you are about to draw ( 1st side = 1 pixel, 2nd side = 2 pixels, etc) --#4.2
+      //        Move the tortoise the length of a side --#4.1
+      Tortoise.move(length);
+      //        Turn the tortoise 1/5th of 360 degrees --#1
+      Tortoise.turn(0.2 * 360);
+      //        Turn the tortoise 1 more degree --#5
+      //    End Repeat --#2.2
+    }
     //    ------------- End of drawPentagon recipe --#10.3
   }
 }
