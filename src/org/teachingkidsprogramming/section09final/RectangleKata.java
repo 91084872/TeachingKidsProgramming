e0570f1b389d94df367b5bf61a5e56a5eda3b041
91084872 <31664295+91084872@users.noreply.github.com>
2018-06-05 09:46:57 -0700

    tried out the RectangleKata

--- src/org/teachingkidsprogramming/section09final/RectangleKata.java
+++ src/org/teachingkidsprogramming/section09final/RectangleKata.java
@@ -1,11 +1,21 @@
 package org.teachingkidsprogramming.section09final;
 
+import org.teachingextensions.logo.Tortoise;
+
 public class RectangleKata
 {
   public static void main(String[] args)
   {
     //    Draw a rectangle
     //    Write the steps to code your rectangle out in English 
+    //          I need to make the tortise move 20 pixels and move to the right 10 pixels then do a for-a-loop 4 times
+    for (int i = 0; i <= 4; i++)
+    {
+      Tortoise.move(20);
+      Tortoise.turn(90);
+      Tortoise.move(10);
+      Tortoise.move(90);
+    }
     //    Translate from English to Java one line at a time
     //    Run your code after each line of Java to make sure it works as expected
     //    XTRA CREDIT: After your code is working, re-factor to remove duplication
